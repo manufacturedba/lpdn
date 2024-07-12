6bb8c13828483d091a979c24d7e1b0d1e86f2c77
Roberto Rodriguez <manufacturedba@gmail.com>
2024-07-12 14:25:36 -0700

    Log out of Azure after Terraform launch

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+az login --service-principal -u $AZ_CLIENT_ID -p $AZ_CLIENT_SECRET --tenant $AZ_TENANT_ID
 terraform init
 TF_VAR_resource_group_name=$RESOURCE_GROUP_NAME terraform apply -input=false -compact-warnings -auto-approve
+az logout
